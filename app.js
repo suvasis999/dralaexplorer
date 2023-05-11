23f54318c984ed557c7d897e4b814fe227c2560b
gopeshed <gopesh.dubey@ed2100.com>
2023-05-11 13:27:16 +0530

    new server code

--- app.js
+++ app.js
@@ -1,124 +1,127 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors =require('cors');
-var debug = require('debug')('rdexplorer:server');
-var http = require('http');
-const bodyPraser=require('body-parser');
+var createError = require('http-errors')
+var express = require('express')
+var path = require('path')
+var cookieParser = require('cookie-parser')
+var logger = require('morgan')
+var cors = require('cors')
+var debug = require('debug')('rdexplorer:server')
+var http = require('http')
+const bodyPraser = require('body-parser')
 
-var indexRouter = require('./routes/index');
-//var usersRouter = require('./routes/users');
+var indexRouter = require('./routes/index')
+// var usersRouter = require('./routes/users');
 
-var app = express();
+var app = express()
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'jade')
 // var port = normalizePort(process.env.PORT || '3002');
 var port = '3002'
 
-app.set('port', port);
+app.set('port', port)
 
-app.use(cors());
-app.use(logger('dev'));
-app.use(express.json());
-app.use(bodyPraser.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.get('/test',function (req,res) {
-        console.log('test');
-return res.status(200).json({
-  success : true})
-        })
-app.use('/', indexRouter);
-//app.use('/users', usersRouter);
+app.use(cors())
+app.use(logger('dev'))
+app.use(express.json())
+app.use(bodyPraser.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+app.get('/test', function (req, res) {
+  console.log('test')
+  return res.status(200).json({
+    success: true
+  })
+})
+app.use('/', indexRouter)
+// app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.use(function (req, res, next) {
+  next(createError(404))
+})
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-var server = http.createServer(app);
+  res.status(err.status || 500)
+  res.render('error')
+})
+// var server = http.createServer(app)
+
 
 /**
  * Listen on provided port, on all network interfaces.
- */
+*/
 
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
 
 /**
  * Normalize a port into a number, string, or false.
- */
+*/
 
-function normalizePort(val) {
-  var port = parseInt(val, 10);
-
+function normalizePort (val) {
+  var port = parseInt(val, 10)
+  
   if (isNaN(port)) {
     // named pipe
-    return val;
+    return val
   }
-
+  
   if (port >= 0) {
     // port number
-    return port;
+    return port
   }
-
-  return false;
+  
+  return false
 }
 
 /**
  * Event listener for HTTP server "error" event.
- */
+*/
 
-function onError(error) {
+function onError (error) {
   if (error.syscall !== 'listen') {
-    throw error;
+    throw error
   }
-
-  var bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
-
+  
+  var bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port
+  
   // handle specific listen errors with friendly messages
   switch (error.code) {
     case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
+      console.error(bind + ' requires elevated privileges')
+      process.exit(1)
+      break
+      case 'EADDRINUSE':
+        console.error(bind + ' is already in use')
+        process.exit(1)
+        break
+        default:
+          throw error
+        }
+      }
+      
+      /**
+       * Event listener for HTTP server "listening" event.
+      */
+     
+function onListening () {
+  var addr = server.address()
+  var bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port
+  debug('Listening on ' + bind)
 }
 
-/**
- * Event listener for HTTP server "listening" event.
- */
+let server = http.createServer({}, app).listen(3002, () => {
+  console.log(`Server running on port ${3002}`)
+})
 
-function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
-  debug('Listening on ' + bind);
-}
+// server.listen(port)
+// server.on('error', onError)
+// server.on('listening', onListening)
 
-module.exports = app;
+module.exports = app
